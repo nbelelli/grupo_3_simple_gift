735ec351ce426c5b23e0f72df588d0751f8c5632
Nicolas Belelli <nbelelli@gmail.com>
2020-10-31 20:56:09 -0300

    controller para la home

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/indexController');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+/* router.get('/', function (req, res) {
+	res.render('index');
+}); */
+
+router.get('/', controller.home);
 
 module.exports = router;
