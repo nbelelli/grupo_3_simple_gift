8fe9960b1d9f6242d1c19f463ccfb47a389eb954
pialeonor12 <jorgeadrian_pucheta@hotmail.com>
2020-11-06 19:05:08 -0300

    cree Node + Express + EJS con Detalle de productos

--- myapp/controllers/productsController.js
+++ myapp/controllers/productsController.js
@@ -2,6 +2,15 @@ const productsController = {
 	products: (req, res) => {
 		res.render('./Products/productsList');
 	},
+	Detail: (req, res) => {
+		res.render('./Products/productDetail');
+	},
 };
 
 module.exports = productsController;
+
+
+
+
+
+;
--- myapp/routes/products.js
+++ myapp/routes/products.js
@@ -4,4 +4,6 @@ const controller = require('../controllers/productsController');
 
 router.get('/', controller.products);
 
+router.get('/productDetail', controller.Detail);
+
 module.exports = router;
