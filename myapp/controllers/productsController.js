9336c1b13df62b68bef4022a7765dd8d11b9155e
Nicolas Belelli <nbelelli@gmail.com>
2020-11-08 00:57:13 -0300

    small ui changes

--- myapp/controllers/productsController.js
+++ myapp/controllers/productsController.js
@@ -1,7 +1,10 @@
 const productsController = {
+	/* Navigates to the products list page */
 	products: (req, res) => {
 		res.render('Products/productsList');
 	},
+
+	/* Navigates to the Create product page */
 	create: (req, res) => {
 		res.render('Products/productCreate');
 	},
