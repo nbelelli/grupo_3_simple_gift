17be5759a09a32d95af73815a67c2dc823453d11
Nicolas Belelli <nbelelli@gmail.com>
2020-11-22 23:11:02 -0300

    Seccion mas elegidos de index dinamica

--- myapp/controllers/indexController.js
+++ myapp/controllers/indexController.js
@@ -1,7 +1,37 @@
+const { renderFile } = require('ejs');
+const fs = require('fs');
+const path = require('path');
+
+const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+
+function getAllProducts() {
+	//Devuelve la DB en un array de objetos literales
+	return JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+}
+
+function writeProducts(productsToSave) {
+	//Recibe un Array de objetos literales.
+	//Lo convierte en un JSON
+	//Reemplaza TODO el json de DB con este neuvo JSON
+	const productsToStringify = JSON.stringify(productsToSave, null, ' ');
+	return fs.writeFileSync(productsFilePath, productsToStringify);
+}
+
+function generateNewId() {
+	const products = getAllProducts();
+	return products.pop().id + 1;
+}
+
 indexController = {
 	home: (req, res) => {
-		res.locals.title = "Home";  
-		res.render('index');
+		res.locals.title = 'Home';
+		const products = getAllProducts();
+		const elegidos = products.filter((product) => {
+			return product.masVendido == true;
+		});
+		console.log(elegidos);
+		res.render('index', { elegidos: elegidos });
 	},
 };
 
