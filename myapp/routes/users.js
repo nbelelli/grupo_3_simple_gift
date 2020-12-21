a54e12332f2651521d0fd0268ca6c517f1dc6dc9
Nicolas Belelli <nbelelli@gmail.com>
2020-12-21 15:47:26 -0300

    auth and guest middlewares

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -4,13 +4,15 @@ const usersController = require('../controllers/usersController');
 const multer = require('multer');
 const path = require('path');
 const validations = require('../middlewares/validations');
+const auth = require('../middlewares/auth');
+const guest = require('../middlewares/guest');
 
 //Multer
 var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'public/images/avatar')
-  },
-  filename: function (req, file, cb) {
+	destination: function (req, file, cb) {
+		cb(null, 'public/images/avatar');
+	},
+	filename: function (req, file, cb) {
 		cb(null, Date.now() + '-' + file.originalname);
 	},
 });
@@ -23,15 +25,15 @@ var upload = multer({ storage: storage });
 });/*
 
 * GET login page. */
-router.get('/login', usersController.login);
+router.get('/login', guest, usersController.login);
 router.post('/login', validations.login, usersController.processLogin);
 
 /* POST register page: User Generator */
 router.post('/register', upload.any(), usersController.storeUser);
 
-router.get('/register', usersController.register);
+router.get('/register', guest, usersController.register);
 
 /* Log Out */
-router.get('/logout', usersController.logout);
+router.get('/logout', auth, usersController.logout);
 
 module.exports = router;
