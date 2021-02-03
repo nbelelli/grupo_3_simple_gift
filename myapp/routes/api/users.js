340b49649e1e0931c2aec964497d87e1bf6c9bc3
brunsen1 <nicolasbrunfman@gmail.com>
2021-02-02 22:47:19 -0300

    FrontValidacionesEsaaaaa

---
+++ myapp/routes/api/users.js
@@ -0,0 +1,10 @@
+/*const express = require('express');
+const router = express.Router();
+const usersApiController = require('../../controllers/api/usersController');
+
+router.get('api/users', usersApiController.list);
+//router.post('/', usersApiController.create);
+
+
+
+module.exports = router;
