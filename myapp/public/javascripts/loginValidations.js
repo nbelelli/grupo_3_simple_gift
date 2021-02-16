ba844aa82a6702f0e8a65cd256bd0854b75c1e71
Nicolas Belelli <nbelelli@gmail.com>
2021-02-16 12:23:58 -0300

    frontEnd validations ui fixes1

--- myapp/public/javascripts/loginValidations.js
+++ myapp/public/javascripts/loginValidations.js
@@ -1,29 +1,32 @@
-//validacion Login 
+//validacion Login
 
-window.addEventListener("load", function(){
-  let formLogin= document.querySelector("form.formLogin");
-  
-  let email = document.querySelector("input.email");
-  let password = document.querySelector("input.password");
+window.addEventListener('load', function () {
+	let formLogin = document.querySelector('form.formLogin');
+	let email = document.querySelector('input.email');
+	let password = document.querySelector('input.password');
 
-  formLogin.addEventListener("submit", function(e){
-    
-    let errores =[];
-    
-    if (email.value == ""){
-      errores.push("El email es obligatorio");
-    }
+	formLogin.addEventListener('submit', function (e) {
+		let errores = [];
+		let ulErrores = document.querySelector('div.errores ul');
 
-    if (password.value == ""){
-      errores.push("La contraseña es obligatorio");
-    }
-    if(errores.length > 0){
-      e.preventDefault();
-     }
-    
-    let ulErrores = document.querySelector("div.errores ul");
-    for(let i=0; i<errores.length; i++){
-      ulErrores.innerHTML += "<li>" + errores[i] + "</li>";
-    }
-  });
-})
+		/*  Comprueba si existen errores */
+		if (ulErrores.innerHTML != '') {
+			ulErrores.innerHTML = '';
+		}
+
+		if (email.value == '') {
+			errores.push('El email es obligatorio');
+		}
+
+		if (password.value == '') {
+			errores.push('La contraseña es obligatorio');
+		}
+		if (errores.length > 0) {
+			e.preventDefault();
+		}
+
+		for (let i = 0; i < errores.length; i++) {
+			ulErrores.innerHTML += '<li>' + errores[i] + '</li>';
+		}
+	});
+});
