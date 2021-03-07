6b8deaae93051ac09f6ad57c7728152940f67e2f
brunsen1 <nicolasbrunfman@gmail.com>
2021-03-07 19:54:01 -0300

    fixValidatorProducts

--- myapp/public/javascripts/productCreateValidations.js
+++ myapp/public/javascripts/productCreateValidations.js
@@ -1,61 +1,70 @@
-window.addEventListener('load', function () {
-	const form = document.querySelector('.createEditForm');
-	console.log(form);
-	const name = document.querySelector('#name');
-	const price = document.querySelector('#price');
-	const discount = document.querySelector('#discount');
-	const image = document.querySelector('#image');
-	const category = document.querySelector('#category');
-	const description = document.querySelector('#description');
-	function getFileExtn(filename) {
-		return filename.split('.').pop();
-	}
+window.addEventListener("load", function () {
+	console.log(window, 'window capturada');
 
-	form.addEventListener('submit', function (e) {
-		let errors = [];
+  
+  const form = document.querySelector(".createEditForm");
+  console.log(form);
+  const name = document.querySelector("#name");
+  const price = document.querySelector("#price");
+  const discount = document.querySelector("#discount");
+  const image = document.querySelector("#image");
+  const category = document.querySelector("#category");
+  const description = document.querySelector("#description");
+   
+  function getFileExtn(filename) {
+	return filename.split('.').pop();
+}
+  form.addEventListener("submit", function (e) {
+    let errores = [];
+    let ulErrores = document.querySelector("div.errores ul");
 
-		let ulErrores = document.querySelector('div.errores ul');
+    /*  Comprueba si existen errores */
+    if (ulErrores.innerHTML != "") {
+      ulErrores.innerHTML = "";
+    }
+    if (name.value == "") {
+		errores.push("El Nombre es obligatorio");
+    }
 
-		/*  Comprueba si existen errores */
-		if (ulErrores.innerHTML != '') {
-			ulErrores.innerHTML = '';
-		}
-		if (name.value == '') {
-			errors.push('El Nombre es obligatorio');
-		}
+    if (price.value == "") {
+		errores.push("El Precio es obligatorio");
+    }
 
-		if (price.value == '') {
-			errors.push('El Precio es obligatorio');
-		}
+    if (discount.value == " ") {
+		errores.push("Si no hay descuento, por favor ingrese 0");
+    }
+    if (discount.value > 99) {
+		errores.push("El maximo descuento es de 99%");
+    }
+    console.log(image.files, "avatar atrapado");
+    console.log(image.files[0]);
+    
+    if (image.value == '') {
+		errores.push("El producto debe tener al menos 1 imagen");
+    }
+     
+    Array.from(image.files).forEach(file => { 
+    debugger;
+    let extn = getFileExtn(file.name);
+    if (!(extn == 'jpg' || extn == 'png' || extn == 'jpeg')) {
+        errores.push('Los formatos correctos son JPG, PGN y JPEG');
+    }
+    });
 
-		if (discount.value == '') {
-			errors.push('Si no hay descuento, por favor ingrese 0');
-		}
 
-		if (discount.value > 99) {
-			errors.push('El maximo descuento es de 99%');
-		}
+	
+    if (description.value == "") {
+		errores.push("La descripción del producto no puede estar vacia");
+    }
 
-		if (image.value == '') {
-			errors.push('El producto debe tener al menos 1 imagen');
-		}
 
-		for (image of image.files) {
-			let extn = getFileExtn(image.name);
-			if (!(extn == 'jpg' || extn == 'png' || extn == 'jpeg')) {
-				errors.push('Los formatos correctos son JPG, PGN y JPEG');
-			}
-		}
+    if (errores.length > 0) {
+      e.preventDefault();
+    }
+	console.log(errores, "contador de errores");
+    for (let i = 0; i < errores.length; i++) {
+		ulErrores.innerHTML += '<li>' + errores[i] + '</li>';
+    }
 
-		if (description.value == '') {
-			errors.push('La descripción del producto no puede estar vacia');
-		}
-
-		if (errors.length > 0) {
-			e.preventDefault();
-			for (error of errors) {
-				ulErrores.innerHTML += '<li>' + error + '</li>';
-			}
-		}
-	});
+  });
 });
--- myapp/public/javascripts/productEditValidations.js
+++ myapp/public/javascripts/productEditValidations.js
@@ -23,30 +23,30 @@ window.addEventListener("load", function () {
       ulErrores.innerHTML = "";
     }
     if (name.value == "") {
-		errores.push("El Nombre es obligatorio A");
+		errores.push("El Nombre es obligatorio");
     }
 
     if (price.value == "") {
-		errores.push("El Precio es obligatorio A");
+		errores.push("El Precio es obligatorio");
     }
 
     if (discount.value == "") {
-		errores.push("Si no hay descuento, por favor ingrese 0 A");
+		errores.push("Si no hay descuento, por favor ingrese 0");
     }
     if (discount.value > 99) {
-		errores.push("El maximo descuento es de 99% A");
+		errores.push("El maximo descuento es de 99%");
     }
     console.log(image.files, "avatar atrapado");
     console.log(image.files[0]);
     if (image.value == '') {
-		errores.push("El producto debe tener al menos 1 imagen A");
+		errores.push("El producto debe tener al menos 1 imagen");
     }
-    for (image of image.files) {
-		let extn = getFileExtn(image.name);
-		if (!(extn == 'jpg' || extn == 'png' || extn == 'jpeg')) {
-			errores.push('Los formatos correctos son JPG, PGN y JPEG A');
-		}
-	}
+    Array.from(image.files).forEach(file => { 
+      let extn = getFileExtn(file.name);
+      if (!(extn == 'jpg' || extn == 'png' || extn == 'jpeg')) {
+          errores.push('Los formatos correctos son JPG, PGN y JPEG');
+      }
+      });
 
 
 	
